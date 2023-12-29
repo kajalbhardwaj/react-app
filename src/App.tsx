8dc4eb177eb9a1f88ae3642d94e1d1f93951fce2
kajal <kabharadwaj@bestpeers.com>
2023-12-29 18:39:18 +0530

    app commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Book from './component/Book';
+import { ViewDetails } from './component/ViewDetails';
+function App() {
+  return (
+    <div>
+       <Book/>
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ src/component/example.tsx
@@ -0,0 +1,61 @@
+// let firstName:string="fruit"
+// let count:number=0
+// let isLoading:boolean=true
+// let numbers:undefined=undefined
+// let age:null|string=null
+
+// // object define
+// const user={
+//   name:"kajal",
+//   age: 24,
+//   id:1
+// }
+
+// // array
+
+// const fruit:string[]=["banana","apple","orange"]
+
+
+// // array of objet
+
+// const users = [{id:1, name:"mmdmcdk"}, {id:1, name:"mkcmkdm"}]
+
+// // function
+
+// const sum=(a:number,b:number)=>{
+//   return a+b
+// }
+
+// sum(1,3)
+
+
+// const handleApiResponse=(payload:{userId:number},callback:()=>void)=>{
+
+// }
+
+
+
+// // interface
+
+// interface User{
+//   name:string,
+//   id:number,
+//   getUser:()=>void
+// }
+
+// interface Person extends User{
+//   department:string
+// }
+
+// let data:User[]=[{
+//   name:"kajal",
+//   id:1,
+//   getUser:()=>{
+    
+//   }
+// }]
+
+
+// type Data={
+  
+// }
